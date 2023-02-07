974f5ec0134b759cfa99609ac5543f082b232da4
Tushar <tushar01kumar28@gmail.com>
2023-02-07 18:21:38 +0530

    first commit

---
+++ handler.js
@@ -0,0 +1,150 @@
+'use strict';
+const AWS = require("aws-sdk");
+const StepFunction = new AWS.StepFunctions();
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const DocumentClient = new DynamoDB.DocumentClient({ region: "ap-northeast-1" })
+
+const isBookAvailable = (book, quantity) => {
+  return (book.quantity - quantity) > 0;
+}
+
+module.exports.checkInventory = async ({ bookId, quantity }) => {
+  try {
+    let params = {
+      TableName: "bookTable",
+      KeyConditionExpression: 'bookId = :bookId',
+      ExpressionAttributeValues: {
+        ':bookId': bookId
+      }
+    }
+
+    let result = await DocumentClient.query(params).promise();
+    let book = result.Items[0];
+
+    if (isBookAvailable(book, quantity)) {
+      return book;
+    } else {
+      let bookOutOfStockError = new Error("The book is out of stock");
+      bookOutOfStockError.name = "BookOutOfStock";
+      throw bookOutOfStockError;
+    }
+
+  } catch (err) {
+    if (err.name === 'BookOutOfStock') throw err;
+    else {
+      let bookNotFoundError = new Error(err);
+      bookNotFoundError.name = "BookNotFound";
+      throw bookNotFoundError;
+    }
+  }
+};
+
+module.exports.calculateTotal = async ({ book, quantity }) => {
+  let total = book.price * quantity;
+  return { total }
+};
+
+const deductPoints = async (userId) => {
+  let params = {
+    TableName: 'userTable',
+    Key: { 'userId': userId },
+    UpdateExpression: 'set points = :zero',
+    ExpressionAttributeValues: {
+      ':zero': 0
+    }
+  }
+
+  await DocumentClient.update(params).promise();
+}
+
+module.exports.redeemPoints = async ({ userId, total }) => {
+  let orderTotal = total.total;
+  try {
+    let params = {
+      TableName: 'userTable',
+      Key: {
+        'userId': userId
+      }
+    }
+
+    let result = await DocumentClient.get(params).promise();
+    let user = result.Item;
+
+    const points = user.points;
+    if (orderTotal > points) {
+      await deductPoints(userId);
+      orderTotal -= points;
+      return { total: orderTotal, points }
+    } else {
+      throw new Error('Order total is less than redeem points');
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+module.exports.billCustomer = async (params) => {
+  // Bill the Customer e.g. using stripe token from the parameters
+  return "Successfully Billed";
+};
+
+module.exports.restoreRedeemPoints = async ({ userId, total }) => {
+  try {
+    if (total.points) {
+      let params = {
+        TableName: "userTable",
+        Key: { userId, userId },
+        Key: { userId: userId },
+        UpdateExpression: 'set points = :points',
+        ExpressionAttributeValues: {
+          ':points': total.points
+        }
+      }
+      await DocumentClient.update(params).promise();
+    }
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+module.exports.sqsWorker = async (event) => {
+  try {
+    console.log(JSON.stringify(event));
+    let record = event.Records[0];
+    var body = JSON.parse(record.body);
+    /** Find a courier and attach courier information to the order */
+    let courier = "<courier email>";
+
+    // update book quantity
+    await updateBookQuantity(body.Input.bookId, body.Input.quantity);
+
+    // throw "Something wrong with Courier API";
+
+    // Attach curier information to the order
+    await StepFunction.sendTaskSuccess({
+      output: JSON.stringify({ courier }),
+      taskToken: body.Token
+    }).promise();
+  } catch (e) {
+    console.log("===== You got an Error =====");
+    console.log(e);
+    await StepFunction.sendTaskFailure({
+      error: "NoCourierAvailable",
+      cause: "No couriers are available",
+      taskToken: body.Token
+    }).promise();
+  }
+}
+
+module.exports.restoreQuantity = async ({ bookId, quantity }) => {
+  let params = {
+    TableName: 'bookTable',
+    Key: { bookId: bookId },
+    UpdateExpression: 'set quantity = quantity + :orderQuantity',
+    ExpressionAttributeValues: {
+      ':orderQuantity': quantity
+    }
+  };
+  await DocumentClient.update(params).promise();
+  return "Quantity restored"
+}
